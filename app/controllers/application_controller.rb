9a00eae0feb70fe3c6b3da63ab5ae6bddcc2fbb7
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-20 13:07:04 -0700

    add basic routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,59 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/directors' do
+    directors = Director.all
+    directors.to_json
+  end
+
+  get '/movies' do
+    movies = Movie.all
+    movies.to_json
+  end
+
+  get '/directors/:id' do
+    director = Director.find(params[:id])
+    director.to_json
+  end
+
+  get '/movies/:id' do
+    movie = Movie.find(params[:id])
+    movie.to_json
+  end
+
+  post '/directors' do
+    director = Director.create(
+      name: params[:name],
+      age: params[:age]
+    )
+    director.to_json
+  end
+
+  post '/movies' do
+    movie = Movie.create(
+      name: params[:name],
+      year: params[:year],
+      RT_score: params[:RT_score],
+      director_id: params[:director_id]
+    )
+    movie.to_json
+  end
+
+  patch '/directors/:id' do
+    director = Director.find(params[:id])
+    director.update(age: params[:age])
+    director.to_json
+  end
+
+  patch '/movies/:id' do
+    movie = Movie.find(params[:id])
+    movie.update(RT_score: [params[:RT_score]])
+    movie.to_json
+  end
+
+  delete 'movies/:id' do
+    movie = Movie.find(params[:id])
+    movie.destroy
   end
 
 end
