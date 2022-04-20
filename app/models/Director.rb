f717585506de20f320a243a474196fdf5b35c2ee
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-20 13:25:13 -0700

    add director instance classes

--- app/models/Director.rb
+++ app/models/Director.rb
@@ -1,3 +1,17 @@
 class Director < ActiveRecord::Base
     has_many :movies
+
+    def average_score
+        scores = self.movies.map {|movie| movie.RT_score}
+        scores.sum / scores.length
+    end
+
+    def highest_rated
+        self.movies.order('RT_score desc').limit(1)
+    end
+
+    def lowest_rated
+        self.movies.order('RT_score asc').limit(1)
+    end
+
 end
