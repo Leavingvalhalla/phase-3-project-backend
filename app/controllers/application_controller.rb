96efb77aa53e92ded165491385c3e2166debcf9d
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-21 08:51:10 -0700

    build requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,59 +1,33 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  get '/directors' do
-    directors = Director.all
-    directors.to_json
+
+  get '/' do
+    Todo.all.to_json
   end
 
-  get '/movies' do
-    movies = Movie.all
-    movies.to_json
-  end
-
-  get '/directors/:id' do
-    director = Director.find(params[:id])
-    director.to_json
-  end
-
-  get '/movies/:id' do
-    movie = Movie.find(params[:id])
-    movie.to_json
-  end
-
-  post '/directors' do
-    director = Director.create(
-      name: params[:name],
-      age: params[:age]
+  post '/' do
+    task = Todo.create(
+      task: params[:task],
+      priority_id: params[:priority_id],
+      label_id: params[:label_id]
     )
-    director.to_json
+    task.to_json
   end
 
-  post '/movies' do
-    movie = Movie.create(
-      name: params[:name],
-      year: params[:year],
-      RT_score: params[:RT_score],
-      director_id: params[:director_id]
+  patch '/:id' do
+    task = Todo.find(params[:id])
+    task.update(
+      task: params[:task],
+      priority_id: params[:priority_id],
+      label_id: params[:label_id]
     )
-    movie.to_json
+    task.to_json
   end
 
-  patch '/directors/:id' do
-    director = Director.find(params[:id])
-    director.update(age: params[:age])
-    director.to_json
-  end
-
-  patch '/movies/:id' do
-    movie = Movie.find(params[:id])
-    movie.update(RT_score: params[:RT_score])
-    movie.to_json
-  end
-
-  delete 'movies/:id' do
-    movie = Movie.find(params[:id])
-    movie.destroy
+  delete '/:id' do
+    task = Todo.find(params[:id])
+    task.destroy
+    task.to_json
   end
 
 end
